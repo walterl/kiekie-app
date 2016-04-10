2abb951f916ab2dcad4e8153a375c127f03b4c3b
Walter Leibbrandt <git wrl co za>
2016-04-11 01:34:46 +0200

    minor: Update imports

--- www/js/index.jsx
+++ www/js/index.jsx
@@ -1,7 +1,7 @@
-import * as ReactDOM from 'react-dom';
-import * as React from 'react';
-import App from './app';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import App from './app';
 
 document.addEventListener('deviceready', () => {
     ReactDOM.render(<App/>, document.getElementById('app'));
--- www/js/picsbox.jsx
+++ www/js/picsbox.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 
 export default class PicsBox extends React.Component {
