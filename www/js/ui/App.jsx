3e6029bc84e522c588e214b49ff8c8ef19a7936c
Walter Leibbrandt <git wrl co za>
2016-07-25 20:16:52 +0200

    Move Redux functionality from photo button to app

--- www/js/ui/App.jsx
+++ www/js/ui/App.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 
 import PicsList from './PicsList';
 import PhotoButton from './PhotoButton';
 
+import {receivePic, takePhoto} from '../actions';
+
 import '../../scss/app.scss';
 
 
-export default class App extends React.Component {
+var debugPic = 0;
+
+/**
+ * Cycle through `1.jpg` through `5.jpg` in the `ignoreme` directory.
+ */
+function nextDebugPic() {
+    debugPic = debugPic % 5 + 1;
+    return `/ignoreme/${debugPic}.jpg`;
+}
+
+
+class App extends React.Component {
     render() {
-        const photoButton = <PhotoButton />;
+        var {onCameraClick} = this.props,
+            photoButton = null;
+        const {debug, dispatch} = this.props;
+
+        if (debug) {
+            onCameraClick = () =>
+                dispatch(receivePic(nextDebugPic(), Date.now()));
+        }
+        photoButton = <PhotoButton onClick={onCameraClick} />;
 
         return (
             <div>
@@ -24,3 +46,25 @@ export default class App extends React.Component {
         );
     }
 }
+
+
+App.propTypes = {
+    debug: React.PropTypes.bool,
+    dispatch: React.PropTypes.func.isRequired,
+    onCameraClick: React.PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+    return {
+        debug: state.config.debug
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onCameraClick: () => dispatch(takePhoto()),
+        dispatch
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- www/js/ui/PhotoButton.jsx
+++ www/js/ui/PhotoButton.jsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import {connect} from 'react-redux';
 
 import IconButton from 'material-ui/IconButton';
 import ImagePhotoCamera from 'material-ui/svg-icons/image/photo-camera';
 
-import {receivePic, takePhoto} from '../actions';
 
-
-var debugPic = 0;
-
-/**
- * Cycle through `1.jpg` through `5.jpg` in the `ignoreme` directory.
- */
-function nextDebugPic() {
-    debugPic = debugPic % 5 + 1;
-    return `/ignoreme/${debugPic}.jpg`;
-}
-
-
-class PhotoButton extends React.Component {
+export default class PhotoButton extends React.Component {
     render() {
-        var onClick = this.props.onClick;
-        if (this.props.debug) {
-            onClick = () =>
-                this.props.dispatch(receivePic(nextDebugPic(), Date.now()));
-        }
         return (
-            <IconButton onClick={onClick}>
+            <IconButton onClick={this.props.onClick}>
                 <ImagePhotoCamera color="white" />
             </IconButton>
         );
@@ -34,21 +15,5 @@ class PhotoButton extends React.Component {
 }
 
 PhotoButton.propTypes = {
-    debug: React.PropTypes.bool,
     onClick: React.PropTypes.func.isRequired
 };
-
-function mapStateToProps(state) {
-    return {
-        debug: state.config.debug
-    };
-}
-
-function mapDispatchToProps(dispatch) {
-    return {
-        onClick: () => dispatch(takePhoto()),
-        dispatch
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PhotoButton);
