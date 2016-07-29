270224bac801bb4c0ddd480cde217688d1ea450e
Walter Leibbrandt <git wrl co za>
2016-07-30 01:19:26 +0200

    Add util functions to perform HTTP get requests and download a file to a temp dir

--- www/js/lib.js
+++ www/js/lib.js
@@ -1,3 +1,29 @@
+var tempFileN = 1;
+
+function httpGet(url) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false);
+    xhr.send(null);
+    return xhr.response;
+}
+
+export function downloadToTemp(url, rootDir, callback) {
+    const data = httpGet(url),
+        filename = `${tempFileN}.jpg`,
+        options = {create: true, exclusive: false};
+    tempFileN += 1;
+
+    rootDir.getDirectory('temp', options, (dirEntry) => {
+        dirEntry.getFile(filename, options, (fileEntry) => {
+            fileEntry.createWriter((writer) => writer.write(data));
+
+            if (typeof callback === 'function') {
+                return callback(fileEntry.toURL());
+            }
+        });
+    });
+}
+
 export function resizeImage(path, options) {
     const dir = options.outputDir || 'default_dir';
     if (path.startsWith('/')) {
