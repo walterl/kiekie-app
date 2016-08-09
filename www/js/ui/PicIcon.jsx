609c570938a82e6f82090bbd9b5b819904b137be
Walter Leibbrandt <git wrl co za>
2016-08-09 23:33:47 +0200

    Use serif font in pic icons

--- www/js/ui/PicIcon.jsx
+++ www/js/ui/PicIcon.jsx
@@ -2,13 +2,15 @@ import React, {PropTypes} from 'react';
 
 import {GridTile} from 'material-ui/GridList';
 
+import '../../scss/picicon.scss';
+
 
 export default class PicIcon extends React.Component {
     render() {
         const {src, onClick} = this.props,
             title = this.props.saved ? 'Saved' : 'NOT Saved';
         return (
-            <GridTile title={title} onClick={onClick}>
+            <GridTile className="pic-icon" onClick={onClick} title={title}>
                 <img src={src} />
             </GridTile>
         );
