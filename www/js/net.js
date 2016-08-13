82c54fecf61a78411e061fa8ceea3f307e19ab31
Walter Leibbrandt <git wrl co za>
2016-08-13 13:47:49 +0200

    Add util function to simplify POSTing JSON data

---
+++ www/js/net.js
@@ -0,0 +1,34 @@
+/**
+ * Checks the HTTP response's status. Any non 2xx status results in an error
+ * being thrown.
+ *
+ * @param {Object} response - HTTP response to check.
+ * @returns {Object} `response` if `response.status` is a 2xx status code.
+ */
+function checkStatus(response) {
+    var error = null;
+    const HTTP_OK = 200,
+        HTTP_REDIRECT = 300;
+
+    if (!(response.status >= HTTP_OK && response.status < HTTP_REDIRECT)) {
+        error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+    }
+
+    return response;
+}
+
+
+export function jsonPost(url, json) {
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(json)
+    })
+    .then(checkStatus)
+    .then((response) => response.json());
+}
