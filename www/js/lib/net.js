accf9461a8ec3ee49b06433bd324f6eefee13d4a
Walter Leibbrandt <git wrl co za>
2016-08-19 00:19:46 +0200

    Parse error messages from response JSON by Promisifying checkStatus()

--- www/js/lib/net.js
+++ www/js/lib/net.js
@@ -1,6 +1,9 @@
 /**
- * Checks the HTTP response's status. Any non 2xx status results in an error
- * being thrown.
+ * Returns a `Promise` that checks the HTTP response's status. Any non 2xx
+ * status results in the promise rejecting with an error. If the reponse status
+ * is good (2xx), the promise resolves with the response.
+ *
+ * This function is meant to be used with the promises returned by `fetch()`.
  *
  * @param {Object} response - HTTP response to check.
  * @returns {Object} `response` if `response.status` is a 2xx status code.
@@ -10,13 +13,27 @@ function checkStatus(response) {
     const HTTP_OK = 200,
         HTTP_REDIRECT = 300;
 
-    if (!(response.status >= HTTP_OK && response.status < HTTP_REDIRECT)) {
-        error = new Error(response.statusText);
-        error.response = response;
-        throw error;
-    }
+    return new Promise((resolve, reject) => {
+        if (!(response.status >= HTTP_OK && response.status < HTTP_REDIRECT)) {
+            error = new Error(response.statusText);
+            error.response = response;
+            // Try and parse JSON from the (error) HTTP response
+            response.json().then((json) => {
+                var messages = [];
 
-    return response;
+                Object.keys(json).forEach((key) => {
+                    messages = [...messages, ...json[key]];
+                });
+                error.messages = messages;
+                error.json = json;
+                reject(error);
+            },
+             // If the JSON parsing fails, fail with the error we have:
+            () => reject(error));
+            return;
+        }
+        resolve(response);
+    });
 }
 
 function buildHeaders(token) {
