03d65f9b83cdbfd21e3f0c828ef3254c526c209e
Walter Leibbrandt <git wrl co za>
2016-08-25 21:38:57 +0200

    Add updateable "API server URL" setting to settings page

--- www/js/ui/Settings.jsx
+++ www/js/ui/Settings.jsx
@@ -2,13 +2,68 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 import AppBar from 'material-ui/AppBar';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
+import {List, ListItem} from 'material-ui/List';
 import NavigationArrowBack from 'material-ui/svg-icons/navigation/arrow-back';
+import Subheader from 'material-ui/Subheader';
+import TextField from 'material-ui/TextField';
 
-import {redirect} from '../actions';
+import {redirect, setConfigUrl} from '../actions';
+
+import '../../scss/settings.scss';
 
 
 class Settings extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {apiDialogOpen: false};
+    }
+
+    setRef(component, ref) {
+        this[ref] = component;
+    }
+
+    toggleState(stateItem) {
+        return () => this.setState({[stateItem]: !this.state[stateItem]});
+    }
+
+    renderApiServerInputDialog() {
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                onTouchTap={this.toggleState('apiDialogOpen')}
+            />,
+            <FlatButton
+                label="Save"
+                primary={true}
+                onTouchTap={() => {
+                    this.props.saveApiServerUrl(this.apiServerInput.getValue());
+                    this.toggleState('apiDialogOpen')();
+                }}
+            />
+        ];
+
+        return (
+            <Dialog
+                title="API Server URL"
+                actions={actions}
+                modal={true}
+                open={this.state.apiDialogOpen}
+                onRequestChange={(o) => this.setState({apiDialogOpen: o})}
+            >
+                <TextField
+                    name="api-server-url-input"
+                    defaultValue={this.props.apiServerUrl}
+                    ref={(c) => this.setRef(c, 'apiServerInput')}
+                    fullWidth={true}
+                />
+            </Dialog>
+        );
+    }
+
     render() {
         const
             menuCloseBtn =
@@ -16,21 +71,39 @@ class Settings extends React.Component {
                     <NavigationArrowBack />
                 </IconButton>;
 
-        return <div>
+        return <div className="settings-page">
             <AppBar
                 title="Settings Kiekie"
                 iconElementLeft={menuCloseBtn}
             />
-            <div>This is the settings page.</div>
+            <div>
+                <List>
+                    <Subheader>Kiekie Server</Subheader>
+                    <ListItem
+                        primaryText="API server URL"
+                        secondaryText={this.props.apiServerUrl}
+                        onTouchTap={this.toggleState('apiDialogOpen')}
+                    />
+                </List>
+            </div>
+
+            {this.renderApiServerInputDialog()}
         </div>;
     }
 }
 
 
+function mapStateToProps(state) {
+    return {
+        apiServerUrl: state.config.urls.api
+    };
+}
+
 function mapDispatchToProps(dispatch) {
     return {
+        saveApiServerUrl: (url) => dispatch(setConfigUrl('api', url)),
         redirectToPics: () => dispatch(redirect('/pics'))
     };
 }
 
-export default connect(null, mapDispatchToProps)(Settings);
+export default connect(mapStateToProps, mapDispatchToProps)(Settings);
