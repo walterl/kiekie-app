892a94921ea61beb3cf89fbffd6619a30381a8df
Walter Leibbrandt <git wrl co za>
2016-08-26 23:51:11 +0200

    Revert "Show note text field in an overlay over the picture"
    This reverts commit 21b8d7cc41f03dd3fefd8386c0469d71b0b55b21.

--- www/js/ui/Pic.jsx
+++ www/js/ui/Pic.jsx
@@ -1,28 +1,25 @@
 import React, {PropTypes} from 'react';
 
-import {darkWhite} from 'material-ui/styles/colors';
 import Card from 'material-ui/Card/Card';
 import CardMedia from 'material-ui/Card/CardMedia';
+import CardText from 'material-ui/Card/CardText';
 import TextField from 'material-ui/TextField';
 
 
 export default class Pic extends React.Component {
     render() {
-        const {note, uri, onNoteChange} = this.props,
-            textField = <TextField
-                floatingLabelText="Note" multiLine={true}
-                value={note}
-                onChange={onNoteChange}
-
-                style={{maxWidth: 'calc(100% - 80px)'}}
-                floatingLabelStyle={{color: darkWhite}}
-                textareaStyle={{color: darkWhite}}
-            />;
+        const {note, uri, onNoteChange} = this.props;
         return (
             <Card className="pic">
-                <CardMedia overlay={textField} >
-                    <img src={uri} />
-                </CardMedia>
+                <CardMedia><img src={uri} /></CardMedia>
+                <CardText>
+                    <TextField
+                        floatingLabelText="Note" multiLine={true}
+                        value={note}
+                        style={{maxWidth: 'calc(100% - 80px)'}}
+                        onChange={onNoteChange}
+                    />
+                </CardText>
             </Card>
         );
     }
