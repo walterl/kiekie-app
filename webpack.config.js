5ebfa4792cfa10fa482b21cffb3b2b3312db16a9
Walter Leibbrandt <git wrl co za>
2016-09-01 01:07:39 +0200

    Make webpack config pass eslint checks

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
+/* global module require __dirname */
+var path = require('path');
+
 module.exports = {
-  entry: ["whatwg-fetch", "./www/js/index.jsx"],
-  output: {
-    path: 'www/build',
-    filename: 'bundle.js'
-  },
-  devtool: "eval",
-  resolve: {
-    extensions: [".js", ".jsx", ""]
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?/,
-        loader: 'babel',
-        include: __dirname + "/www/js",
-        query: {
-          presets: [
-            require.resolve("babel-preset-es2015"),
-            require.resolve("babel-preset-react")
-          ]
-        }
-      },
-      {
-        test: /\.scss/,
-        loaders: ['style', 'css', 'sass']
-      }
-    ]
-  }
+    entry: ['whatwg-fetch', './www/js/index.jsx'],
+    output: {
+        path: 'www/build',
+        filename: 'bundle.js'
+    },
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx', '']
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?/,
+            loader: 'babel',
+            include: path.join(__dirname, '/www/js'),
+            query: {
+                presets: [
+                    require.resolve('babel-preset-es2015'),
+                    require.resolve('babel-preset-react')
+                ]
+            }
+        }, {
+            test: /\.scss/,
+            loaders: ['style', 'css', 'sass']
+        }]
+    }
 };
